c38667e6c997646eb025fdcb626685e64a84838c
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-07-12 20:50:36 +0500

    b oraliqda

--- logic01.py
+++ logic01.py
@@ -1,7 +1,8 @@
 def main(a,b,c):
-    return a<b and b<c
+    return (b>a and b<c) or (b<a and b>c)
 print(main(2,3,4))
 print(main(6,4,5))
+print(main(7,6,5))
 """
     Given three integers a, b, c,  check the following statement "The number b is between a and c".
     Args:
